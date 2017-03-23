7e23faddaa655b8719e7e421ce9ad26b0518a485
Martin Wenge <martin.wengenmayr@mailbox.tu-dresden.de>
2017-03-23 23:23:47 +0100

    documentation to header files

--- GenericFullHeader.h
+++ GenericFullHeader.h
@@ -3,6 +3,15 @@
 #ifndef GENERIC_FULL_H
 #define GENERIC_FULL_H
 
+/** @file GenericFullHeader.h
+ * @author Martin Wengenmayr
+ * @date 23.03.2017
+ * @brief summary of includes
+ * 
+ * @todo to be removed 
+ * when dependencies are fully resolved in the source files
+ */
+
 #include <SFML/System.hpp>
 #include <SFML/Graphics.hpp>
 #include <SFML/Window.hpp>
--- windowSize.h
+++ windowSize.h
@@ -1,8 +1,17 @@
 #ifndef WINDOW_SIZE_H
 #define WINDOW_SIZE_H
 
+/** @file windowSize.h
+ * @author Martin Wengenmayr
+ * @date 23.03.2017
+ * @brief definiton of window sizes
+ * 
+ * @details change window Size between 800x600 or 1024x768
+ */
+
 //screen variables
     /* uncomment here: */
+    //! screen width, to be changed in 
     const static int SCREEN_WIDTH = 800;
     const static int SCREEN_HEIGHT = 600;
     /* */
